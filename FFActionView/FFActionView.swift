56044a0e1ceef7d107fb4ef754c5a1447ff5c0c9
飞飞 <sf2907989@163.com>
2017-12-29 22:12:28 +0800

    项目初始化

---
+++ FFActionView/FFActionView.swift
@@ -0,0 +1,21 @@
+//
+//  FFActionView.swift
+//  FFActionViewExample
+//
+//  Created by 飞飞 on 2017/12/29.
+//  Copyright © 2017年 飞飞. All rights reserved.
+//
+
+import UIKit
+
+class FFActionView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
